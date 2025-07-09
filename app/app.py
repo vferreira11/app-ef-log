790d56f3aa988600b508976664e7fb3f2e77e306
vferreira11 <ferreira.vinicius11@gmail.com>
2025-07-08 23:16:54 -0300

    wip

--- app/app.py
+++ app/app.py
@@ -188,7 +188,7 @@ if st.button("GERAR SIMULAÇÃO"):
             yaxis=dict(title='Profundidade (mm)', range=[0, profundidade_cel]),
             zaxis=dict(title='Altura (mm)', range=[0, altura_cel]),
             aspectmode='data',
-            camera=dict(eye=dict(x=-2, y=2, z=2))
+            camera=dict(eye=dict(x=-1.5, y=2, z=1)
         ),
         margin=dict(l=0, r=0, t=0, b=0),
         showlegend=False
