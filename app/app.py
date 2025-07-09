8f1f0aedb8c4ce9dedeb9aec0f9b148a7c1da9be
vferreira11 <ferreira.vinicius11@gmail.com>
2025-07-08 21:40:18 -0300

    continuar zoom 3d

--- app/app.py
+++ app/app.py
@@ -188,7 +188,7 @@ if st.button("GERAR SIMULAÇÃO"):
         # câmera com perspectiva e zoom inicial idêntico ao original
         camera=dict(
             projection=dict(type='perspective'),
-            eye=dict(x=0, y=9, z=0)
+            eye=dict(x=2.2, y=-2.2, z=1.2)
         ),
         xaxis=dict(title='Largura (mm)'),
         yaxis=dict(title='Profundidade (mm)'),
